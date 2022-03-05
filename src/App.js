b62370a90530b5219c39e1e38c1c7b903e01407e
matsuo-develop-mitsu <matsuo.develop+mitsu@gmail.com>
2022-03-05 12:41:47 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 function App() {
+  const Users = [
+    {
+      name: "Taro",
+      age: 23,
+    },
+    {
+      name: "Hanako",
+      age: 34,
+    },
+  ];
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      {Users.map((user, index) => {
+        return <User key={index} name={user.name} age={user.age} />;
+      })}
+    </>
   );
 }
+
+const User = (props) => {
+  const { name, age } = props;
+  return (
+    <div>
+      Hi, I'm {name} , and {age} years old
+    </div>
+  );
+};
 
 export default App;
